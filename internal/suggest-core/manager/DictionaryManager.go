1c652ad1616ff1ca7aea6ac0fba84037e47d50dd
yangshujie <yangshujie@fangcunyisheng.com>
2023-05-26 19:36:02 +0800

    fix 初始化 manager 模型

--- internal/suggest-core/manager/DictionaryManager.go
+++ internal/suggest-core/manager/DictionaryManager.go
@@ -1,1 +1,23 @@
 package manager
+
+import "github.com/yshujie/suggest/internal/suggest-core/model"
+
+// DictionaryManager 词典管理器
+type DictionaryManager struct {
+	wBank model.WordBank
+}
+
+// CreateDictionary 创建词典
+func (m *DictionaryManager) CreateDictionary(code string) (model.Dictionary, error) {
+	return model.Dictionary{}, nil
+}
+
+// InitDictionary 初始化词典
+func (m *DictionaryManager) InitDictionary(dic model.Dictionary) error {
+	return nil
+}
+
+// DestroyDictionary 销毁词典
+func (m *DictionaryManager) DestroyDictionary(dic model.Dictionary) error {
+	return nil
+}
--- internal/suggest-core/manager/WordBackManager.go
+++ internal/suggest-core/manager/WordBackManager.go
@@ -1,1 +1,21 @@
 package manager
+
+import "github.com/yshujie/suggest/internal/suggest-core/model"
+
+// WordBankManager 词库管理器
+type WordBankManager struct{}
+
+// CreateWordBack 创建词库
+func (m *WordBankManager) CreateWordBack(code string) (model.WordBank, error) {
+	return model.WordBank{}, nil
+}
+
+// InitWordBank 初始化词库
+func (m *WordBankManager) InitWordBank(wordBank model.WordBank) error {
+	return nil
+}
+
+// DestroyWordBack 销毁词库
+func (m *WordBankManager) DestroyWordBack(bank model.WordBank) error {
+	return nil
+}

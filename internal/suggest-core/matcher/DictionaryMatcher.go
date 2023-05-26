3a1ac7d04ddb4bf65cc227a0f55eb80d99fa9296
yangshujie <yangshujie@fangcunyisheng.com>
2023-05-26 19:38:11 +0800

    fix 初始化 matcher 模型

--- internal/suggest-core/matcher/DictionaryMatcher.go
+++ internal/suggest-core/matcher/DictionaryMatcher.go
@@ -1,1 +1,11 @@
 package matcher
+
+import "github.com/yshujie/suggest/internal/suggest-core/model"
+
+type DictionaryMatcher struct {
+}
+
+// Match 根据关键词匹配词典
+func (m *DictionaryMatcher) Match(keyword string) (model.Dictionary, error) {
+	return model.Dictionary{}, nil
+}

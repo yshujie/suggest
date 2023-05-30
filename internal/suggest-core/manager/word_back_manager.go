9680d9108f5bc65ceb0e27943671a7fa146e7fbc
yangshujie <yangshujie@fangcunyisheng.com>
2023-05-30 09:03:42 +0800

    fix 更新文件名称，由大驼峰法更改为下划线法

--- internal/suggest-core/manager/word_back_manager.go
+++ internal/suggest-core/manager/word_back_manager.go
@@ -3,7 +3,10 @@ package manager
 import "github.com/yshujie/suggest/internal/suggest-core/model"
 
 // WordBankManager 词库管理器
-type WordBankManager struct{}
+type WordBankManager struct {
+	code        string           `json:"code,omitempty"`
+	dictionarys model.Dictionary `json:"dictionarys,omitempty"`
+}
 
 // CreateWordBack 创建词库
 func (m *WordBankManager) CreateWordBack(code string) (model.WordBank, error) {

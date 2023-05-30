d400f3b0e673e95430f3d58383792d63cb079ad1
yangshujie <yangshujie@fangcunyisheng.com>
2023-05-30 09:27:38 +0800

    fix entry 中新增 ChildEntryObject, PatientEntryObject, DoctorEntryObject, HospitalEntryObject

--- internal/suggest-core/model/entry.go
+++ internal/suggest-core/model/entry.go
@@ -15,3 +15,80 @@ type EntryObject interface {
 	ToString() string
 	ToJson() string
 }
+
+type ChildEntryObject struct {
+	Code   string
+	Id     int
+	Name   string
+	Mobile string
+}
+
+func (o *ChildEntryObject) GetCode() string {
+	return o.Code
+}
+
+func (o *ChildEntryObject) ToString() string {
+	return ""
+}
+
+func (o *ChildEntryObject) ToJson() string {
+	return ""
+}
+
+type PatientEntryObject struct {
+	Code   string
+	Id     int
+	Name   string
+	Mobile string
+}
+
+func (o *PatientEntryObject) GetCode() string {
+	return o.Code
+}
+
+func (o *PatientEntryObject) ToString() string {
+	return ""
+}
+
+func (o *PatientEntryObject) ToJson() string {
+	return ""
+}
+
+type DoctorEntryObject struct {
+	Code     string
+	Id       int
+	Name     string
+	Mobile   string
+	Hospital string
+}
+
+func (o *DoctorEntryObject) GetCode() string {
+	return o.Code
+}
+
+func (o *DoctorEntryObject) ToString() string {
+	return ""
+}
+
+func (o *DoctorEntryObject) ToJson() string {
+	return ""
+}
+
+type HospitalEntryObject struct {
+	Code   string
+	Id     int
+	Name   string
+	Mobile string
+}
+
+func (o *HospitalEntryObject) GetCode() string {
+	return o.Code
+}
+
+func (o *HospitalEntryObject) ToString() string {
+	return ""
+}
+
+func (o *HospitalEntryObject) ToJson() string {
+	return ""
+}
